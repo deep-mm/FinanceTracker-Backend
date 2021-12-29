5b1f54d5eb893c439caaca49c1cf26d8e69b66d8
deep-mm <deep.mm@somaiya.edu>
2021-12-29 13:03:45 +0530

    Update post deployment script

--- FinancialTracker.Database/Scripts/Script.PostDeployment1.sql
+++ FinancialTracker.Database/Scripts/Script.PostDeployment1.sql
@@ -9,8 +9,8 @@ Post-Deployment Script Template
                SELECT * FROM [$(TableName)]					
 --------------------------------------------------------------------------------------
 */
-/*SET IDENTITY_INSERT [dbo].[InvestmentStatus] ON 
-GO */
+SET IDENTITY_INSERT [dbo].[InvestmentStatus] ON 
+GO
 INSERT [dbo].[InvestmentStatus] ([InvestmentStatusId], [InvestmentStatusName], [CreatedBy], [CreatedDate], [ModifiedBy], [ModifiedDate], [IsActive]) VALUES (1, N'Draft', N'Deep Mehta', CAST(N'2021-12-28T14:53:50.783' AS DateTime), N'Deep Mehta', CAST(N'2021-12-28T14:53:50.783' AS DateTime), 1)
 GO
 INSERT [dbo].[InvestmentStatus] ([InvestmentStatusId], [InvestmentStatusName], [CreatedBy], [CreatedDate], [ModifiedBy], [ModifiedDate], [IsActive]) VALUES (2, N'Active', N'Deep Mehta', CAST(N'2021-12-28T14:53:50.783' AS DateTime), N'Deep Mehta', CAST(N'2021-12-28T14:53:50.783' AS DateTime), 1)
@@ -18,8 +18,8 @@ GO
 INSERT [dbo].[InvestmentStatus] ([InvestmentStatusId], [InvestmentStatusName], [CreatedBy], [CreatedDate], [ModifiedBy], [ModifiedDate], [IsActive]) VALUES (3, N'Archived', N'Deep Mehta', CAST(N'2021-12-28T14:53:50.783' AS DateTime), N'Deep Mehta', CAST(N'2021-12-28T14:53:50.783' AS DateTime), 1)
 GO
 
-/* SET IDENTITY_INSERT [dbo].[InvestmentType] ON 
-GO */
+SET IDENTITY_INSERT [dbo].[InvestmentType] ON 
+GO
 INSERT [dbo].[InvestmentType] ([InvestmentTypeId], [InvestmentTypeName], [CreatedBy], [CreatedDate], [ModifiedBy], [ModifiedDate], [IsActive]) VALUES (1, N'Stocks', N'Deep Mehta', CAST(N'2021-12-28T14:53:50.783' AS DateTime), N'Deep Mehta', CAST(N'2021-12-28T14:53:50.783' AS DateTime), 1)
 GO
 INSERT [dbo].[InvestmentType] ([InvestmentTypeId], [InvestmentTypeName], [CreatedBy], [CreatedDate], [ModifiedBy], [ModifiedDate], [IsActive]) VALUES (2, N'Mutual Fund', N'Deep Mehta', CAST(N'2021-12-28T14:53:50.783' AS DateTime), N'Deep Mehta', CAST(N'2021-12-28T14:53:50.783' AS DateTime), 1)
@@ -33,8 +33,8 @@ GO
 INSERT [dbo].[InvestmentType] ([InvestmentTypeId], [InvestmentTypeName], [CreatedBy], [CreatedDate], [ModifiedBy], [ModifiedDate], [IsActive]) VALUES (6, N'Others', N'Deep Mehta', CAST(N'2021-12-28T14:53:50.783' AS DateTime), N'Deep Mehta', CAST(N'2021-12-28T14:53:50.783' AS DateTime), 1)
 GO
 
-/* SET IDENTITY_INSERT [dbo].[Member] ON 
-GO */
+SET IDENTITY_INSERT [dbo].[Member] ON 
+GO 
 INSERT [dbo].[Member] ([MemberId], [MemberName], [CreatedBy], [CreatedDate], [ModifiedBy], [ModifiedDate], [IsActive]) VALUES (1, N'Manoj Mehta', N'Deep Mehta', CAST(N'2021-12-28T14:53:50.783' AS DateTime), N'Deep Mehta', CAST(N'2021-12-28T14:53:50.783' AS DateTime), 1)
 GO
 INSERT [dbo].[Member] ([MemberId], [MemberName], [CreatedBy], [CreatedDate], [ModifiedBy], [ModifiedDate], [IsActive]) VALUES (2, N'Bina Mehta', N'Deep Mehta', CAST(N'2021-12-28T14:53:50.783' AS DateTime), N'Deep Mehta', CAST(N'2021-12-28T14:53:50.783' AS DateTime), 1)

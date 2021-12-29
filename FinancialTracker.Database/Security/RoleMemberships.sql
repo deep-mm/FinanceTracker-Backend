1e16762dbe1c9c3e0e7d26a169c1839e13f5fced
deep-mm <deep.mm@somaiya.edu>
2021-12-29 12:57:51 +0530

    Update script

--- FinancialTracker.Database/Security/RoleMemberships.sql
+++ FinancialTracker.Database/Security/RoleMemberships.sql
@@ -3,11 +3,3 @@ CREATE ROLE db_executor;
 GO
 GRANT EXECUTE TO db_executor;
 GO
-
-create user [financetracker-api] from external provider;
-GO
-alter role db_datareader add member [financetracker-api];
-GO
-alter role db_datawriter add member [financetracker-api];
-GO
-alter role db_executor add member [financetracker-api];
